1ae56a0053bdc7f8c12744c8efc16a26cc12e008
Jvtta2008 <trinhdo.svic@gmail.com>
2017-01-21 19:43:51 +1300

    Add Rename

--- userSetup.py
+++ userSetup.py
@@ -1,3 +1,8 @@
+#gate for mayaCharm
+
 import maya.cmds as cmds
+
+print "check port 7002 for mayaCharm communication - this is for developer"
+
 if not cmds.commandPort(':7002', q=True):
     cmds.commandPort(n=':7002')
